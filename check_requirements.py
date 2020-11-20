44849fd13382003dbe1113c519c2ddaab0067b09
Furkan Bostancı <steetstyle@gmail.com>
2020-11-20 18:36:59 +0300

    fixed some mistake in check_requirements.py

--- check_requirements.py
+++ check_requirements.py
@@ -7,7 +7,7 @@ import runpy
 def main(argv):
     os.system('sudo apt-get install python3-pip')
     os.system('sudo pip3 install --upgrade OPi.GPIO')
-    os.system('sudo pip3 install ./libraries/spi_py')
+    os.system('sudo pip3 install ./libraries/SPI-Py')
 
 if __name__ == "__main__":
    main(sys.argv[1:])
