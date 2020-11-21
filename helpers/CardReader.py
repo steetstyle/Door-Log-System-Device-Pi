8ba0b8b38cea0ed8ab0cacd2e8294684c4158ca0
Furkan Bostancı <steetstyle@gmail.com>
2020-11-21 15:52:18 +0300

    pin chnge

--- helpers/CardReader.py
+++ helpers/CardReader.py
@@ -27,8 +27,8 @@ class CardReader(object):
 			self.tag += "1"	
 
 	def registerReader(self, edge = RPIO.FALLING, pull_up_down=RPIO.PUD_UP):
-		RPIO.setup(self.GPIO_0, RPIO.IN)
-		RPIO.setup(self.GPIO_1, RPIO.IN)
+		RPIO.setup(self.GPIO_0, pull_up_down)
+		RPIO.setup(self.GPIO_1, pull_up_down)
 		RPIO.add_event_detect(self.GPIO_0, edge, callback=self.addBitToTag)
 		RPIO.add_event_detect(self.GPIO_1, edge, callback=self.addBitToTag)
 		 
