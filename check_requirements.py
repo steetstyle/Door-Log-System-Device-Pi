9af56f823b0a2d573028a946e195f7a37c222362
Furkan Bostancı <steetstyle@gmail.com>
2020-12-16 00:02:13 +0300

    requrements fixed

--- check_requirements.py
+++ check_requirements.py
@@ -5,6 +5,9 @@ import subprocess
 import importlib
 import runpy
 def main(argv):
+    os.system('sudo apt-get update -y')
+    os.system('sudo apt-get install python3-dev -y')
+    os.system('pip3 install wheel')
     os.system('sudo apt-get install python3-pip python3-setuptools python3-psutil python3-bottle python3-requests python3-dev libzbar-dev libzbar0 -y')
     os.system('sudo pip3 install --upgrade OPi.GPIO')
     os.system('sudo pip3 install ./libraries/SPI-Py')
