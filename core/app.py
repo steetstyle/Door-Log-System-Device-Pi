d0e2f31665919e3e466b29d5024643deac41f8f1
Furkan Bostancı <steetstyle@gmail.com>
2020-12-19 23:13:15 +0300

    port changed

--- core/app.py
+++ core/app.py
@@ -37,7 +37,7 @@ class App:
         GPIO.setmode(GPIO.BOARD)
 
         self.readersList = [
-            CardReader("reader", PORT., 12, self.TIMEOUT),
+            CardReader("reader", 16, 12, self.TIMEOUT),
         ]
 
         #Starting readers
