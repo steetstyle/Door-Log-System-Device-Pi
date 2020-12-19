b3dbd6053d5931f51374b4253b53f4d4d4db1c05
Furkan Bostancı <steetstyle@gmail.com>
2020-12-19 23:08:42 +0300

    DoorLogSystemPi

--- core/app.py
+++ core/app.py
@@ -6,13 +6,20 @@ from helpers.read_wifi_credentials import ReadWifiCredentials
 from config import channel_list
 import OPi.GPIO as GPIO
 from time import sleep
-from helpers.MFRC522 import MFRC522
+from helpers.CardReader import CardReader
 import signal
 
 class App:
 
     continue_reading = False
-    MIFAREReader = MFRC522()
+
+    BIT_TRANSMISSION_TIME = 0.002 #From wiegand specification
+    FRAMESIZE = 48 #Supposed size of received frame
+    FRAMETIME = FRAMESIZE * BIT_TRANSMISSION_TIME #Theoric time necessary to transfer a frame
+    ALLOWANCE = 10 #Auhtorized allowance for the transmission time in percent
+    TIMEOUT = FRAMETIME*(1+ALLOWANCE/100) #Real time allowed for the transmission
+
+    reader_list = []
 
     def __init__(self):
         self.setup()
@@ -23,58 +30,27 @@ class App:
                 password=wifi_credentials['password'],
                 interface=wifi_credentials['interface_name'])
         F.run()
-        
-        ''' 
-        GPIO.setup(channel_list.high_channel_in_list, GPIO.IN, initial=GPIO.LOW)
-        GPIO.setup(channel_list.high_channel_out_list, GPIO.OUT, initial=GPIO.HIGH)
-        GPIO.setup(channel_list.low_channel_in_list, GPIO.IN, initial=GPIO.LOW)
-        GPIO.setup(channel_list.low_channel_out_list, GPIO.OUT, initial=GPIO.HIGH) 
-        '''
 
         # Hook the SIGINT
         signal.signal(signal.SIGINT, self.end_read)
         self.continue_reading = True
+        GPIO.setmode(GPIO.BOARD)
 
+        self.readersList = [
+            CardReader("reader", PORT., 12, self.TIMEOUT),
+        ]
+
+        #Starting readers
+        readersCount = 1
+        for reader in self.readersList:
+            print("Initializing reader " + str(readersCount) + "...")
+            reader.registerReader()
+            print(" Done !")
+            readersCount += 1
 
     def run(self):
-        try:
-            print("Press CTRL+C to exit")
-            # This loop keeps checking for chips. If one is near it will get the UID and authenticate
-            while self.continue_reading:
-                # Scan for cards    
-                (status,TagType) = self.MIFAREReader.MFRC522_Request(self.MIFAREReader.PICC_REQIDL)
-                
-                # If a card is found
-                if status == self.MIFAREReader.MI_OK:
-                    print("Card detected")
-                
-                (status, uid) = self.MIFAREReader.MFRC522_Anticoll()
-                
-                # If we have the UID, continue
-                if status == self.MIFAREReader.MI_OK:
-                    # Print UID
-                    print("Card read UID: "+str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3]))
-
-                # This is the default key for authentication
-                key = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
-
-                # Select the scanned tag
-                self.MIFAREReader.MFRC522_SelectTag(uid)
-
-                # Authenticate
-                status = self.MIFAREReader.MFRC522_Auth(self.MIFAREReader.PICC_AUTHENT1A, 8, key, uid)
-
-                # Check if authenticated
-                if status == self.MIFAREReader.MI_OK:
-                    self.MIFAREReader.MFRC522_Read(8)
-                    self.MIFAREReader.MFRC522_StopCrypto1()
-                else:
-                    print("Authentication error")
-
-        except KeyboardInterrupt:
-            GPIO.output(7, 0)           # set port/pin value to 0/LOW/False
-            GPIO.cleanup()              # Clean GPIO
-            print("Bye.")
+        while self.continue_reading:
+            pass        
             
     def end_read(self,signal,frame):
         print("Ctrl+C captured, ending read.")
